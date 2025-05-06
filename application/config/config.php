8009aaa8042e12ecc4ffc30b03eb3ed7fbe3fdd4
kinjal.softnoesis <kinjal.softnoesis@gmail.com>
2025-05-06 14:59:49 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-// $config['base_url'] = 'https://www.softnoesis.com/leaves';
 
 $config['base_url'] = '';
 /*
